758c250ed645ced71de1305b314e6dc83e014944
b-sea <>
2024-03-18 23:16:42 -0400

    finishing domain layer

--- internal/domain/recipe/repository.go
+++ internal/domain/recipe/repository.go
@@ -5,8 +5,15 @@ import "github.com/google/uuid"
 
 // Repository defines all functions required to interact with recipes.
 type Repository interface {
-	GetByOwnerIDs(ids []uuid.UUID) ([]*Recipe, error)
+	Find(owners []uuid.UUID, filter *Filter) ([]*Recipe, error)
+	GetOne(id uuid.UUID) (*Recipe, error)
 	Create(recipe *Recipe) error
 	Update(recipe *Recipe) error
 	Delete(id uuid.UUID) error
+}
+
+// Filter is a search filter for recipes.
+type Filter struct {
+	Name *string
+	Tags *[]Tag
 }
