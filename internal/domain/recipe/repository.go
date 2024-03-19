64b18937e868ec734342b44056d67f2627cf3c29
b-sea <>
2024-03-18 22:37:15 -0400

    add domain repo

--- internal/domain/recipe/repository.go
+++ internal/domain/recipe/repository.go
@@ -1,2 +1,12 @@
 // Package recipe defines everything to manage the recipes domain.
 package recipe
+
+import "github.com/google/uuid"
+
+// Repository defines all functions required to interact with recipes.
+type Repository interface {
+	GetByOwnerIDs(ids []uuid.UUID) ([]*Recipe, error)
+	Create(recipe *Recipe) error
+	Update(recipe *Recipe) error
+	Delete(id uuid.UUID) error
+}
